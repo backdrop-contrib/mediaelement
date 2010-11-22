3430ab57896233277a2677459efbb1bf626f2be5
Matt Farina <matt@mattfarina.com>
2010-11-22 16:11:27 +0000

    Updated to proper dependency injection.

--- mediaelement.js
+++ mediaelement.js
@@ -1,12 +1,12 @@
 // $Id$
-;(function($, undefined){
+(function($, Drupal, undefined){
   /**
    * When set to enable mediaelement for all audio/video files add it to the page.
    */
   Drupal.behaviors.mediaelement = {
     attach: function(context, settings) {
-      if (Drupal.settings.mediaelement != undefined) {
-        $.each(Drupal.settings.mediaelement, function(selector, options) {
+      if (settings.mediaelement !== undefined) {
+        $.each(settings.mediaelement, function(selector, options) {
           $(selector, context).once('mediaelement', function() {
             if (options.controls) {
               $(this).mediaelementplayer(options);
@@ -15,15 +15,15 @@
               $(this).mediaelement();
             }
           });
-        })
+        });
       }
       // The global option is seperate from the other selectors as it should be
       // run after any other selectors.
-      if (Drupal.settings.mediaelementAll != undefined) {
+      if (settings.mediaelementAll !== undefined) {
         $('video,audio', context).once('mediaelement', function() {
           $(this).mediaelementplayer();
         });
       }
     }
-  }
-})(jQuery);
+  };
+})(jQuery, Drupal);
